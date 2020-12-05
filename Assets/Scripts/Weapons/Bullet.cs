03cf7eda3f87016a0fd50434b88014c7fe31b879
Riley <Riley@ipartneruniforms.com>
2020-12-05 14:35:41 -0800

    Added a second weapon and some better organization for bullet objects

---
+++ Assets/Scripts/Weapons/Bullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+	
+	private float lifetime = 0.0f;
+	
+	void Update() {
+		
+		lifetime += Time.deltaTime;
+		
+		if(lifetime > 5.0f)
+			Destroy(gameObject);
+		
+	}
+   
+	void OnTriggerEnter(Collider other) {
+	 
+		//Destroy(gameObject);
+	 
+	}
+   
+}
