66d476ea3091f6c2894fe49fb4fb6546d49c900e
Riley <Riley@ipartneruniforms.com>
2020-12-08 01:36:17 -0800

    Populated upgrades menu

--- Assets/Scripts/PersistantData.cs
+++ Assets/Scripts/PersistantData.cs
@@ -7,10 +7,52 @@ public class PersistantData : MonoBehaviour
     
 	public int credits = 0;
 	
+	/* Permanent Upgrades */
+	public int handgunDamage = 1;
+	public int handgunDamageUpgrades = 0;
+	public int handgunReload = 1;
+	public int handgunReloadUpgrades = 0;
+	
+	public int automaticDamage = 1;
+	public int automaticDamageUpgrades = 0;
+	public int automaticReload = 1;
+	public int automaticReloadUpgrades = 0;
+	
+	public int shotgunDamage = 1;
+	public int shotgunDamageUpgrades = 0;
+	public int shotgunReload = 1;
+	public int shotgunReloadUpgrades = 0;
+	
+	public int moveSpeed = 1;
+	public int moveSpeedUpgrades = 0;
+	public int creditsMult = 1;
+	public int creditsMultUpgrades = 0;
+	/* ------------------ */
+	
 	
 	// Reset all data
 	public void resetData() {
 		credits = 0;
+		
+		handgunDamage = 1;
+		handgunDamageUpgrades = 0;
+		handgunReload = 1;
+		handgunReloadUpgrades = 0;
+		
+		automaticDamage = 1;
+		automaticDamageUpgrades = 0;
+		automaticReload = 1;
+		automaticReloadUpgrades = 0;
+		
+		shotgunDamage = 1;
+		shotgunDamageUpgrades = 0;
+		shotgunReload = 1;
+		shotgunReloadUpgrades = 0;
+		
+		moveSpeed = 1;
+		moveSpeedUpgrades = 0;
+		creditsMult = 1;
+		creditsMultUpgrades = 0;
 	}
 	
 	void Awake() {
