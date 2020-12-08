897ac50643c660a35e0f2c69a36c081bb411846c
Riley <Riley@ipartneruniforms.com>
2020-12-08 00:11:18 -0800

    Added button hover feedback to main menu

---
+++ Assets/Scripts/UI/MainMenuButton.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MainMenuButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+	
+	public Text thisText;
+	
+	
+	private int blue;
+	private int red;
+	private bool dir;
+	
+	void Start() {
+		
+		thisText = transform.GetChild(0).GetComponent<Text>();
+		
+	}
+	
+	
+	public void OnPointerEnter(PointerEventData eventData) {
+		thisText.color = new Color(0, 0, 190.0f / 255);
+		blue = 190;
+		red = 0;
+		dir = true;
+		StartCoroutine("Glow");
+	}
+	
+	public void OnPointerExit(PointerEventData eventData) {
+		StopCoroutine("Glow");
+		thisText.color = new Color(50.0f / 255, 50.0f / 255, 50.0f / 255);
+	}
+	
+	
+	IEnumerator Glow() {
+		
+		while(true) {
+			
+			if(dir) {
+				blue--;
+				red++;
+				
+				if(blue <= 0)
+					dir = false;
+			} else {
+				blue++;
+				red--;
+				
+				if(red <= 0)
+					dir = true;
+			}
+			
+			thisText.color = new Color(red / 255.0f, 0, blue / 255.0f);
+			
+			yield return new WaitForSecondsRealtime(0.005f);
+		}
+		
+	}
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour {
+	
+	private Camera mainCamera;
+	
+	private int position;
+	private float panTarget;
+	
+	void Start() {
+		
+		mainCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
+		
+		position = 0;
+		
+	}
+	
+	public void panLeft() {
+		if(position <= -1)
+			return;
+		
+		position--;
+		
+		// Pan 10 units to the left
+		panTarget = -10.0f;
+		StartCoroutine("smoothPan");
+	}
+	
+	public void panRight() {
+		if(position >= 1)
+			return;
+		
+		position++;
+		
+		// Pan 10 units to the left
+		panTarget = 10.0f;
+		StartCoroutine("smoothPan");
+	}
+	
+	IEnumerator smoothPan() {
+		
+		for(int i = 0; i < 100; i++) {
+			mainCamera.transform.Translate(panTarget / 100, 0, 0);
+			
+			yield return new WaitForSecondsRealtime(0.0008f);
+		}
+		
+		yield return null;
+		
+	}
+	
+}
