bb7a77248ba40302a27c74e6735baa2f12400807
Samuel Saenz <ssaenz@sdsu.edu>
2020-12-08 14:17:28 -0800

    Added shop script

---
+++ Assets/Scripts/Shop.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shop : MonoBehaviour
+{
+    public Canvas shopUI;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shopUI.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            if (shopUI.enabled == false)
+            {
+                openShop();
+            }
+        }
+    }
+
+    void openShop()
+    {
+        Time.timeScale = 0;
+        Cursor.lockState = CursorLockMode.Confined;
+        shopUI.enabled = true;
+    }
+
+    public void closeShop()
+    {
+        Time.timeScale = 1;
+        shopUI.enabled = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void buyAutomatic()
+    {
+        Player.hasAutomatic = true;
+    }
+    public void buyShotgun()
+    {
+        Player.hasShotgun = true;
+    }
+    public void buySword()
+    {
+        Player.hasSword = true;
+    }
+
+
+
+}
