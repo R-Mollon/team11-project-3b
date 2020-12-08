a793e681fa5483fdef1778f22a7291adb2bdc7c3
Riley <Riley@ipartneruniforms.com>
2020-12-08 01:08:55 -0800

    Added upgrades menu

---
+++ Assets/Scripts/PersistantData.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistantData : MonoBehaviour
+{
+    
+	public int credits = 0;
+	
+	
+	// Reset all data
+	public void resetData() {
+		credits = 0;
+	}
+	
+	void Awake() {
+		GameObject[] objects = GameObject.FindGameObjectsWithTag("PersistantData");
+
+        if (objects.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+	
+}
