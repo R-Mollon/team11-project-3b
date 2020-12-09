56fb92287346e024afe21697e54d4361ece3166b
Riley <Riley@ipartneruniforms.com>
2020-12-08 19:56:11 -0800

    Replaced terrain in main menu to remove error from previous terrain

--- Assets/Scripts/UI/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour {
 	
@@ -85,6 +86,10 @@ public class MainMenuController : MonoBehaviour {
 		
 		yield return null;
 		
+	}
+	
+	public void startGame() {
+		SceneManager.LoadScene(1);
 	}
 	
 	public void quitGame(int ignoreLevel) {
